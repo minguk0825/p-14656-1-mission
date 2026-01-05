74a61bf9148c5eceec760e6c338f6ed8b6bc7338
minguk0825 <mapletrue01@naver.com>
2026-01-05 16:33:28 +0900

    0012

--- src/main/java/com/back/domain/post/post/service/PostService.java
+++ src/main/java/com/back/domain/post/post/service/PostService.java
@@ -5,6 +5,8 @@ import com.back.domain.post.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class PostService {
@@ -18,4 +20,9 @@ public class PostService {
         Post post = new Post(title, content, author);
         return postRepository.save(post);
     }
+
+    public List<Post> findAll() {
+        return postRepository.findAll();
+    }
+
 }
